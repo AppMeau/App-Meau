8d8f51e8221b123024a0225a5697f4bbb04ea9fc
Caio Peluti <cpeluti@gmail.com>
2024-04-07 21:25:34 -0300

    botão do google adicionado (WIP)

--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ButtonComponent from '../components/button';
+import Zocial from '@expo/vector-icons/Zocial'
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <ButtonComponent link='/login' type='positive'>Entrar</ButtonComponent>
-      <StatusBar style="auto" />
+      <ButtonComponent link='/login' type='positive'>ENTRAR</ButtonComponent>
+      <ButtonComponent link='/login' type='google'><View><Zocial name='googleplus' /><Text>ENTRAR COM GOOGLE</Text></View></ButtonComponent>
     </View>
   );
 }
--- components/button.tsx
+++ components/button.tsx
@@ -1,33 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+
+
 
 type componentProps = {
-    type: 'positive',
+    type: 'positive' | 'google' | 'facebook',
     link: string,
-    children: string
+    children: string | JSX.Element | JSX.Element[]
 }
 export default function ButtonComponent(props: componentProps) {
+    let [fontsLoaded, fontError] = useFonts({
+        Roboto_400Regular,
+    });
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
   return (
     <Link
-        style={styles[props.type]}
+        style={{...styles[props.type], ...styles.button}}
         href={props.link}
     >{props.children}</Link>
   );
 }
 
 const styles = StyleSheet.create({
-  positive: {
-    fontFamily: 'roboto',
-    backgroundColor: '#88c9bf',
-    width: 232,
-    height: 40,
-    borderRadius: 2,
-    color: '#434343',
-    fontSize: 12,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    button: {
+        width: 232,
+        display:'flex',
+        flexDirection:"row",
+        height: 40,
+        borderRadius: 2,
+        fontSize: 12,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Roboto_400Regular',
+    },
+    positive: {
+        backgroundColor: '#88c9bf',
+        color: '#434343',
+    },
+    google: {
+        backgroundColor: '#f15f5c',
+        color: '#f7f7f7',
+    },
+    facebook: {
+        backgroundColor: '#194f7c',
+        color: '#f7f7f7',
+    }
 });
