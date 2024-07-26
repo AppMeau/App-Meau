d5fd8edf87a606005bb073800460a815f1e52015
Caio Peluti <cpeluti@gmail.com>
2024-07-25 21:03:52 -0300

    Migração para uso do expo-constants

--- app.config.js
+++ app.config.js
@@ -1,4 +1,5 @@
-{
+import 'dotenv/config';
+export default {
   "expo": {
     "name": "appmeau",
     "slug": "appmeau",
@@ -37,6 +38,12 @@
       ]
     ],
     "extra": {
+      EXPO_PUBLIC_API_KEY: process.env.EXPO_PUBLIC_API_KEY,
+      EXPO_PUBLIC_AUTH_DOMAIN: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+      EXPO_PUBLIC_PROJECT_ID: process.env.EXPO_PUBLIC_PROJECT_ID,
+      EXPO_PUBLIC_STORAGE_BUCKET: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+      EXPO_PUBLIC_APP_ID: process.env.EXPO_PUBLIC_APP_ID,
+      
       "router": {
         "origin": false
       },
